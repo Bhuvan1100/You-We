27efca91621869b2cdae27a17029475b3fec0fb5
Bhuvan <bhaskarbhuvan2004@gmail.com>
2025-06-26 10:36:44 +0530

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='bg-blue-100'>hellow this is a chat app</div> 
+    </>
+  )
+}
+
+export default App
