693c02db5b826a56fae9ae167110f2cf8fa8ec7b
Bhuvan <bhaskarbhuvan2004@gmail.com>
2025-07-26 22:54:23 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import { handleSocketConnection } from './socket/oneToOneHnadler.js';
 import { handleGroupChat } from './socket/groupChatHandler.js';
 import {handlePersonalizedRoom} from './socket/handlePersonalizedRoom.js';
 import authRoutes from './routes/auth.js';
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
